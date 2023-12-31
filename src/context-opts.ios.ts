65986025d7e8e331aec8bcbf8fb6c71dade24325
Benny Nottonson <bennynottonson@gmail.com>
2023-12-31 12:48:31 -0800

    Init app

---
+++ src/context-opts.ios.ts
@@ -0,0 +1,18 @@
+import { Platform } from 'react-native'
+
+export default {
+  useCoreMLIos: true,
+  // If you don't want to enable Core ML, you can remove this property
+  coreMLModelAsset:
+    Platform.OS === 'ios'
+      ? {
+          filename: 'ggml-base-encoder.mlmodelc',
+          assets: [
+            require('../assets/ggml-base-encoder.mlmodelc/weights/weight.bin'),
+            require('../assets/ggml-base-encoder.mlmodelc/model.mil'),
+            require('../assets/ggml-base-encoder.mlmodelc/coremldata.bin'),
+          ],
+        }
+      : undefined,
+  useGpu: false, // Enable Metal (Will skip Core ML if enabled)
+}
